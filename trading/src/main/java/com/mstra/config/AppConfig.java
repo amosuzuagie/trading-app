b9981d8151902f4d11faddefaf173a9877c18f2e
Amos Uzuagie <amosuzuagie@gmail.com>
2025-04-02 02:45:02 +0100

    chore(cors): configure CORS for cross-origin requests

--- trading/src/main/java/com/mstra/config/AppConfig.java
+++ trading/src/main/java/com/mstra/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.mstra.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,7 +8,12 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class AppConfig {
@@ -24,6 +30,20 @@ public class AppConfig {
     }
 
     private CorsConfigurationSource corsConfigurationSource() {
-        return null;
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(
+                        Arrays.asList("http://localhost:5173", "http://localhost:3000")
+                );
+                config.setAllowedMethods(Collections.singletonList("*"));
+                config.setAllowCredentials(true);
+                config.setExposedHeaders(List.of(("Authorization")));
+                config.setAllowedHeaders(Collections.singletonList("*"));
+                config.setMaxAge(3600L);
+                return config;
+            }
+        };
     }
 }
